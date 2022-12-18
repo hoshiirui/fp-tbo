9a1e5f2ddf6c529d06feb9f4e13c2215ec6d7f6c
Okiya Rui <120825371+hoshiirui@users.noreply.github.com>
2022-12-18 22:03:43 +0800

    Add files via upload

---
+++ handlers/routes.py
@@ -0,0 +1,24 @@
+from flask import render_template, request
+from modules import modules
+from flask import request, jsonify
+from flask_cors import cross_origin
+from modules import modules, cyk
+
+def configure_routes(app):
+    @app.route("/")
+    def index():
+        hello = modules.hello()
+        content = modules.content()
+        return render_template("index.html", hello=hello, content=content)
+
+    @app.route("/app", methods=["GET", "POST"])
+    def app():
+        if (request.method == "POST"):
+            string = request.form["sentenceinput"]
+            result = cyk.is_accepted(string)
+            return render_template("app.html", result=result, submit=True, string=string)
+        else:
+            return render_template("app.html", submit=False)
+
+        
+    
