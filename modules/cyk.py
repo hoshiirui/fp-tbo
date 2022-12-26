e3f8074d62786e10757cacbb752034c64cea10d4
Okiya Rui <120825371+hoshiirui@users.noreply.github.com>
2022-12-26 21:42:45 +0800

    Update cyk.py
    var localization

--- modules/cyk.py
+++ modules/cyk.py
@@ -1,18 +1,18 @@
-from modules.cnf import get_set_of_production
+import modules.cnf
 
-TRIANGULAR_TABLE = {}
+tabelsegitiga = {}
 g = None
 previousNode = None
 
-def is_accepted(inputString):
-    global TRIANGULAR_TABLE
-    TRIANGULAR_TABLE.clear()
-    prodRules = get_set_of_production()
+def getValid(inputString):
+    global tabelsegitiga
+    tabelsegitiga.clear()
+    prodRules = cnf.setOfProduction()
     temp = inputString.lower().split(" ")
     inputString = temp
     for i in range(1,len(inputString)+1):
         for j in range(i, len(inputString)+1):
-            TRIANGULAR_TABLE[(i,j)] = []
+            tabelsegitiga[(i,j)] = []
     for i in reversed(range(1, len(inputString)+1)):
         for j in range(1, i+1):
             if (j == j + len(inputString) - i):
@@ -21,13 +21,13 @@ def is_accepted(inputString):
                     for val in value:
                         if (val == inputString[j-1] and key not in tempList):
                             tempList.append(key)
-                TRIANGULAR_TABLE[(j, j + len(inputString) - i)] = tempList
+                tabelsegitiga[(j, j + len(inputString) - i)] = tempList
             else:
                 tempList = []
                 resultList = []
                 for k in range(len(inputString) - i):
-                    first = TRIANGULAR_TABLE[(j,j+k)]
-                    second = TRIANGULAR_TABLE[(j+k+1,j+len(inputString) - i)]
+                    first = tabelsegitiga[(j,j+k)]
+                    second = tabelsegitiga[(j+k+1,j+len(inputString) - i)]
                     for fi in first:
                         for se in second:
                             if (fi + " " + se not in tempList):
@@ -36,11 +36,28 @@ def is_accepted(inputString):
                     for val in value:
                         if (val in tempList and key not in resultList):
                             resultList.append(key)
-                TRIANGULAR_TABLE[(j,j+len(inputString) - i)] = resultList
-    if "K" in TRIANGULAR_TABLE[(1, len(inputString))]:
-        return "Valid"
+                tabelsegitiga[(j,j+len(inputString) - i)] = resultList
+    if "K" in tabelsegitiga[(1, len(inputString))]:
+        return True
     else:
-        return "Tidak Valid"
+        return False
 
-print(is_accepted("Alex bercita-cita menjadi pilot"))
+def getTable(inputString):
+    global tabelsegitiga
+    result = []
+    n = len(inputString.split(" "))
+    for i in range(1, n+1):
+        temp = []
+        for j in range(i):
+            res = tabelsegitiga[(j+1, n-i+j+1)]
+            if len(res) == 0:
+                temp.append("\u2205")
+            else:
+                temp.append("{" + ", ".join(res) + "}")
+        result.append(temp)
+    result.append(inputString.split(" "))
+    return result
+
+print(getValid("Alex bercita-cita menjadi pilot"))
+print(*getTable("Alex bercita-cita menjadi pilot"), sep="\n")
 #print(TRIANGULAR_TABLE)
