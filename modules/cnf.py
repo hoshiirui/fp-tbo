712fa15fee12b39d467008a76b121995e2b9dcdb
Okiya Rui <120825371+hoshiirui@users.noreply.github.com>
2022-12-26 21:43:32 +0800

    Update cnf.py
    localization

--- modules/cnf.py
+++ modules/cnf.py
@@ -1,48 +1,48 @@
-RESULT = {}
+hasil = {}
 
-def remove_unit_production(keyList):
-    global RESULT
-    for key, value in RESULT.items():
+def removeUnitProduction(keyList):
+    global hasil
+    for key, value in hasil.items():
         if key in keyList:
             tempList = []
             for prod in value:
                 if len(prod.split(" ")) == 2:
                     tempList.append(prod)
                 else:
-                    for i in RESULT[prod]:
+                    for i in hasil[prod]:
                         if i not in tempList:
                             tempList.append(i)
-            RESULT[key] = tempList
+            hasil[key] = tempList
 
-def get_set_of_production():
-    global RESULT
-    RESULT.clear()
+def setOfProduction():
+    global hasil
+    hasil.clear()
     f = open("./variable_list.txt", "r", encoding="utf-8")
     for lines in f:
         line = lines.splitlines()
         line = line[0].split(" -> ")
         lhs = line[0]
         rhs = line[1].split(" | ")
-        if lhs in RESULT.keys():
-            RESULT[lhs].extend(rhs)
+        if lhs in hasil.keys():
+            hasil[lhs].extend(rhs)
         else:
-            RESULT[lhs] = rhs
+            hasil[lhs] = rhs
     f.close()
-    for key, value in RESULT.items():
+    for key, value in hasil.items():
         if key == "PropNoun":
             tempList = []
             for val in value:
                 if val not in tempList:
                     tempList.append(val.lower())
-            RESULT[key] = tempList
+            hasil[key] = tempList
     phrases = ["NumP", "AdvP", "AdjP", "PP", "NP", "VP"]
-    remove_unit_production(phrases)
+    removeUnitProduction(phrases)
     patterns = ["S", "P", "O", "Pel", "Ket"]
-    remove_unit_production(patterns)
+    removeUnitProduction(patterns)
     tempList = []
     tempDict = {}
     counter = 1
-    for key, value in RESULT.items():
+    for key, value in hasil.items():
         if key == "K":
             for val in value:
                 if len(val.split(" ")) > 2:
@@ -66,7 +66,7 @@ def get_set_of_production():
                     tempList.append(" ".join(temp))
                 else:
                     tempList.append(val)
-            RESULT[key] = tempList
+            hasil[key] = tempList
     for key, value in tempDict.items():
-        RESULT[key] = [value]
-    return RESULT
+        hasil[key] = [value]
+    return hasil
